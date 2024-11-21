0cd4e01cc5002db0805c875cc3fdcfe968e220be
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-22 00:24:47 +0700

    ss3 bai 5

---
+++ ss3 bai 5.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+
+int main()
+{
+	printf("                               Danh Sach Sinh Vien                                             \n  ");
+	
+	printf("|Stt|   Ho va Ten   |     Tuoi  |       SDT              |          mail            |\n");
+	
+	printf("_______________________________________________________________________________________________\n");
+	
+	printf("| 1 |  Nguyen van A |     19    |        0123456789        |   vn@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 2 |  Nguyen Van B |     18    |        0123456789        |   bahb@gmail.com         |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 3 |  Nguyen Van C |     27    |        0356584545        |   ab@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 4 |  Nguyen van D |     25    |        0123456789        |   cn@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 5 |  Nguyen Van E |     25    |        0123456789        |   ok@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 6 |  Nguyen Van G |     27    |        0356584545        |   hk@gmail.com           |\n");
+	return 0;
+	
+}
